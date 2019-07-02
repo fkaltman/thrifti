4cbd5caf6dc27598b91c2692d35492f63caa1fda
Francine Altman <francinekline@gmail.com>
2019-07-02 12:01:47 -0400

    zips rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { fetchStores } from "./services/stores";
 import StoreCards from './components/StoreCards';
 import EnterZip from './components/EnterZip';
 import { Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+// import, render and pass it stores
 
 
 class App extends React.Component {
@@ -25,21 +27,26 @@ class App extends React.Component {
 
   handleSubmit = (ev) => {
     ev.preventDefault();
+    // passes this.state.zip as an argument to handleSearch
     this.handleSearch(this.state.zip);
   }
 
   handleSearch = async (zipCode) => {
     const data = await fetchStores(zipCode);
     console.log(this.state.stores)
+    this.setState({
+      stores: data.businesses
+    })
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Link className="home" to="/">thrifti</Link>
+          <Home />
+          {/* <Link className="home" to="/">thrifti</Link> */}
           <Link className="zipz" to="/enter-zip"></Link>
-
+          <StoreCards stores={this.state.stores} />
         </header>
         <div id="display">
           <Route
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import StoreCards from './StoreCards';
+
+
 
 export default function Home() {
 
   return (
     <>
+
       <div>
         <Link className="stores" to="/stores-by-zip"></Link>
       </div>
       <section>
         <Route
-          path="/stores-by-zip" render={() => <StoreCards stores={this.state.stores} />} />
+          path="/stores-by-zip" render={() => <StoreCards stores={this.props.stores} />} />
       </section>
     </>
   )
