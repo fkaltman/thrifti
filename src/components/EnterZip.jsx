c7bc953b27d88b42d273fd7b7c118b16d5d65dd9
Francine Altman <francinekline@gmail.com>
2020-03-05 15:11:56 -0500

    added button to main page and changed fonts

--- src/components/EnterZip.jsx
+++ src/components/EnterZip.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Shelves from '../img/annie-spratt-unsplash.jpg'
 import Rack from '../img/becca-mchaffie-unsplash.jpg'
+
 export default function EnterZip(props) {
 
   return (
     <div className="zip-page">
-      <div id="zip-prompt"> find thrift stores nearby...</div>
           <img className="zip-image-shelves"
             src={Shelves} />
           <img className="zip-image-rack"
@@ -25,6 +25,8 @@ export default function EnterZip(props) {
             <button id="search-button">Search</button>
           </form>
         </div>
+        <h6 className="credit-clothes-rack">Photo by Becca McHaffie on Unsplash</h6>
+        <h6 className="credit-shelves">Photo by Annie Spratt on Unsplash</h6>
       </section >
     </div >
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import StoreCards from './StoreCards';
+import Hanger from "../img/thriftcycle-trans.png";
 
 export default function Home() {
 
   return (
     <div>
       <div id="homescreen">
-        <Link to="/enter-zip" className="main-thrifty">thrifti</Link>
+        <h3 className="main-title">thrifti</h3>
+        <h4 className="main-title2">find thrift stores nearby</h4>
+        <Link to="/enter-zip">
+        <img className="hanger-button"
+            src={Hanger} />
+        </Link>
+        <h6 className="credit-stack">Photo by Victoria Strukovskaya on Unsplash</h6>
       </div>
     </div>
   )
